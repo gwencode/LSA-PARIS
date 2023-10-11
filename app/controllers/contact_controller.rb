4f4daae851ac7e46680629dde5db32a0d64d0230
gwendal le bris <gwen.coding@gmail.com>
2023-10-11 11:10:09 +0200

    Feature: replace recaptcha v3 by v2 i'm not a rebot if v3 failed

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,12 +11,13 @@ class ContactController < ApplicationController
     authorize @contact
     # Captcha
     @contact.validate # this line will validate the user even if Recaptcha failed. This way we will present all potential validation errors right away
-    check = verify_recaptcha action: 'message', minimum_score: 0.7, secret_key: ENV['RECAPTCHA_SECRET_KEY']
+    v3_verify = verify_recaptcha action: 'message', minimum_score: 0.9, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3']
+    v2_verify = verify_recaptcha(secret_key: ENV['RECAPTCHA_SECRET_KEY_V2'])
 
-    if check && @contact.valid?
+    if (v3_verify || v2_verify) && @contact.valid?
       ContactMailer.with(contact: @contact).contact_admin_email.deliver_now
       ContactMailer.with(contact: @contact).contact_user_email.deliver_now
-      redirect_to contact_path, notice: "Votre message a bien été envoyé."
+      redirect_to root_path, notice: "Votre message a bien été envoyé."
     else
       render :new, status: :unprocessable_entity
     end
